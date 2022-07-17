be7e067f5408e0e7d6fef3358eb69da4a4216bbb
Alfian Luthfi <alfiankurniawan85@gmail.com>
2022-07-17 11:32:25 +0700

    Add Simple Style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+import "./style/App.css";
 import { Profile } from "./components/Profile";
 import { RecentWork } from "./components/RecentWork";
 
 function App() {
     return (
-        <div className=''>
+        <div className='App'>
             <h1>Halo Dunia</h1>
             <Profile name='Luthfi' umur='20' />
             <RecentWork />
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Profile = (props) => {
     return (
         <div>
-            <h1>Ini Profil nya {props.name}</h1>
+            <h1 style={{ color: "Red" }}>Ini Profil nya {props.name}</h1>
             <p>Umurnya {props.umur}</p>
         </div>
     );
